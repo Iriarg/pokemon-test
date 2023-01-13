b802f3c2c89f984bf2c8b7c6b77d7b3cf2218d04
Iria <iriargon94@gmail.com>
2023-01-13 11:03:29 +0100

    random names done

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>¿Quién es este Pokemon?</h1>
+    <PokemonPicture :pokemonId="600" :showPokemon="true"/>
+
+    <PokemonOptions :pokemons="pokemonArr"/>
+
+  
+</template>
+
+<script>
+    import PokemonPicture from '@/components/PokemonPicture.vue'
+    import PokemonOptions from '@/components/PokemonOptions.vue'
+
+    import getPokemonOptions from '../helpers/getPokemonOptions'
+
+    // console.log(getPokemonOptions())
+
+export default {
+
+  components: { PokemonPicture, PokemonOptions },
+  data() {
+    return {
+      pokemonArr:[]
+    }
+  },
+  methods: {
+    async mixPokemonArr() {
+      this.pokemonArr = await getPokemonOptions()
+    }
+  },
+  mounted() {
+    this.mixPokemonArr()
+  }
+
+}
+</script>
+
